cd846c051134b2131745b7665fbce0b37789f97f
wangxuwei84 <wangxuwei84@gmail.com>
2025-11-25 14:45:55 +0800

    + add breadcrumbs for workspace and project page

--- frontends/web/src/views/v-project.ts
+++ frontends/web/src/views/v-project.ts
@@ -1,8 +1,9 @@
 import { position } from '@dom-native/draggable';
 import { getRouteOrgId, pathAsNum, pathAt } from 'common/route';
 import { BaseViewElement } from 'common/v-base.js';
-import { projectDco } from 'dcos';
+import { orgDco, projectDco, wksDco } from 'dcos';
 import { append, closest, customElement, elem, first, getAttr, on, OnEvent, onEvent, onHub } from 'dom-native';
+import { Org, Wks } from 'shared/entities';
 import { Project } from 'shared/entities/project-entity.js';
 import { asNum } from 'utils-min';
 
@@ -74,8 +75,10 @@ export class projectListView extends BaseViewElement {
 		if ((this.hasPathChanged(2) || force) && pathAt(1)) {
 			const projectId = pathAsNum(2);
 			if(!projectId || force){
+				const wks = await wksDco.get(this.wksId!);
+				const org = await orgDco.get(wks.orgId!);
 				const projectList = await projectDco.list({matching: {wksId: this.wksId!}});
-				this.innerHTML = _render(this.wksId!, projectList);
+				this.innerHTML = _render(org, wks, projectList);
 			}else{
 				this.innerHTML = `<v-project-main project-id='${projectId}'></v-project-main>`;
 			}
@@ -85,8 +88,16 @@ export class projectListView extends BaseViewElement {
 
 //// HTMLs
 
-function _render(wksId: number, projectList: Project[] = []) {
-	let html = `	<header><h1>Projects</h1></header>
+function _render(org:Org, wks: Wks, projectList: Project[] = []) {
+	let html = `
+	<div class="breadcrumbs">
+		<a href="/">Organizations</a>
+		<span class="sep">/</span>
+		<a href="/${org.id}">${org.name}</a>
+		<span class="sep">/</span>
+		<span>${wks.name}</span>
+	</div>
+	<header><h1>Projects</h1></header>
 	<section>
 		<div class="card project-add">
 			<c-ico src="#ico-add"></c-ico>
@@ -95,7 +106,7 @@ function _render(wksId: number, projectList: Project[] = []) {
 	`;
 	const orgId = getRouteOrgId();
 	for (const p of projectList) {
-		html += `	<a class="card project" data-type="Project" data-id="${p.id}" href="/${orgId}/${wksId}/${p.id}">
+		html += `	<a class="card project" data-type="Project" data-id="${p.id}" href="/${orgId}/${wks.id}/${p.id}">
 		<header>
 			<h2>${p.name}</h2>
 			<c-ico src="#ico-more" class="show-menu"></c-ico>
--- frontends/web/src/views/v-wks.ts
+++ frontends/web/src/views/v-wks.ts
@@ -1,9 +1,9 @@
 import { position } from '@dom-native/draggable';
 import { getRouteOrgId, pathAsNum, pathAt } from 'common/route';
 import { BaseViewElement } from 'common/v-base.js';
-import { wksDco } from 'dcos';
+import { orgDco, wksDco } from 'dcos';
 import { append, closest, customElement, elem, first, getAttr, on, OnEvent, onEvent, onHub } from 'dom-native';
-import { Wks } from 'shared/entities.js';
+import { Org, Wks } from 'shared/entities.js';
 import { asNum } from 'utils-min';
 
 @customElement('v-wks')
@@ -75,8 +75,9 @@ export class wksListView extends BaseViewElement {
 		if ((this.hasPathChanged(1) || force) && pathAt(0)) {
 			const wksId = pathAsNum(1);
 			if(!wksId || force){
+				const org = await orgDco.get(this.orgId!);
 				const wksList = await wksDco.list({matching: {orgId: this.orgId!}});
-				this.innerHTML = _render(wksList);
+				this.innerHTML = _render(org, wksList);
 			}else{
 				this.innerHTML = `<v-project wks-id='${wksId}'></v-project>`;
 			}
@@ -86,9 +87,15 @@ export class wksListView extends BaseViewElement {
 
 //// HTMLs
 
-function _render(wksList: Wks[] = []) {
-	let html = `	<header><h1>Workspaces</h1></header>
-	<section>
+function _render(org:Org,wksList: Wks[] = []) {
+	let html = `
+		<div class="breadcrumbs">
+			<a href="/">Organizations</a>
+			<span class="sep">/</span>
+			<span>${org.name}</span>
+		</div>
+		<header><h1>Workspaces</h1></header>
+		<section>
 		<div class="card wks-add">
 			<c-ico src="#ico-add"></c-ico>
 			<h3>Add New Workspace</h3>
